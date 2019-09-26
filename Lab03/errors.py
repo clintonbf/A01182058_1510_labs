b2a68481b96af187eb31e44f1637a3ec8d7f56ae
clintonbf <cfernandes24@my.bcit.ca>
2019-09-26 14:54:47 -0700

    Completed errors.py

--- Lab03/errors.py
+++ Lab03/errors.py
@@ -1,2 +1,33 @@
+import sys
+
+
+def display_div_error():
+    print("Div by 0 example")
+    print(1 / 0)
+
+
+def display_index_error_1():
+    print("Index error 1")
+    word = "error"
+    print(word[5])
+
+
+def display_index_error_2():
+    print("Index error 2")
+    print(sys.argv[1])
+
+
+def display_type_error():
+    print("Type error")
+    print("The number is " + 5)
+
+
 def main():
-    print("Hello world")
+    display_div_error()
+    # display_index_error_1()
+    # display_index_error_2()
+    # display_type_error()
+
+
+if __name__ == "__main__":
+    main()
