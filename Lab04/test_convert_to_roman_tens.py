c44dbfd22587068b826295e44b9f17d6dc258f0b
clintonbf <cfernandes24@my.bcit.ca>
2019-09-30 16:04:47 -0700

    test_convert_to_roman_tens(): minor call correction

--- Lab04/test_convert_to_roman_tens.py
+++ Lab04/test_convert_to_roman_tens.py
@@ -9,12 +9,10 @@ class TestConvert_to_roman_tens(TestCase):
         self.assertEqual(roman_numbers.convert_to_roman_tens(4), "XL")
 
     def test_convert_to_roman_tens_eq_5(self):
-        self.assertEqual(roman_numbers.convert_to_roman_ones(5), "L")
+        self.assertEqual(roman_numbers.convert_to_roman_tens(5), "L")
 
     def test_convert_to_roman_tens_gt_5(self):
         self.assertEqual(roman_numbers.convert_to_roman_tens(7), "LXX")
 
     def test_convert_to_roman_tens_eq_9(self):
         self.assertEqual(roman_numbers.convert_to_roman_tens(9), "XC")
-
-
