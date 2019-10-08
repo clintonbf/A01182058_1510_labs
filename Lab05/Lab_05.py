4a945cde899ea40375f8dd139851c5bd10eadf18
clintonbf <cfernandes24@my.bcit.ca>
2019-10-07 18:12:19 -0700

    Most code generation complete

--- Lab05/Lab_05.py
+++ Lab05/Lab_05.py
@@ -53,3 +53,175 @@ def roll_die(number_of_rolls, number_of_sides):
             number_of_sides = number_of_sides - 1
 
     return roll_sum
+
+
+def choose_inventory(inventory, selection):
+    """
+    Provide a random selection of inventory.
+
+    :param inventory: list
+    :param selection: int
+    :precondition: selection is int, > 0
+    :postcondition: generates a sorted, random selection of inventory
+    :return: list of strings
+    """
+
+    gear_taken = []
+
+    if len(inventory) == 0 or selection == 0:
+        pass
+    elif selection < 0:
+        print("Error: selection < 0 is NOT permitted.")
+    else:
+        if selection > len(inventory):
+            print("Getting greedy. Venger would be proud.")
+            gear_taken = sorted(inventory)
+        elif selection == len(inventory):
+            gear_taken = sorted(inventory)
+        else:
+            iteration_count = 0
+            for i in range(0, selection):
+                gear_choice = random.choice(inventory)
+
+                if iteration_count > (selection * 5):
+                    i = selection  # we've been at this long enough; break out
+                    gear_taken.append("My hat fell off so I short-changed you.")
+                else:
+                    if gear_choice in gear_taken:
+                        i = (i - 1)  # decrement i because we couldn't take this gear
+                        iteration_count = (iteration_count + 1)
+                    else:
+                        gear_taken.append(gear_choice)
+
+    return sorted(gear_taken)
+
+
+def generate_vowel():
+    """
+    Return a randomly-selected vowel (incl. y).
+
+    :postcondition: single vowel randomly selected
+    :return: string
+    """
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+    which_vowel = random.choice(vowels)
+
+    return which_vowel
+
+
+def generate_consonant():
+    """
+     Return a randomly-selected consonant (incl. y).
+
+    :postcondition: single consonant randomly selected
+    :return: string
+    """
+    consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y',
+                 'z']
+
+    return random.choice(consonant)
+
+
+def generate_syllable():
+    """
+    Generate a consonant-vowel pair
+
+    :postcondition: consonant-vowel pair created
+    :return: string
+    """
+
+    return generate_consonant() + generate_vowel()
+
+
+def generate_name(syllables):
+    """
+    Generate string of consonant-vowel pairs.
+
+    :param syllables: int, > 0
+    :precondition: syllables is an int, > 0
+    :postcondition: generates a syllables consonant-vowel pairs
+    :return: list
+    """
+
+    syllable_list = []
+
+    for i in range(0, syllables):
+        syllable_list.append(generate_syllable())
+
+    return syllable_list
+
+
+def create_character(name_length):
+    """
+    Generate a DnD character.
+
+    :param name_length: int
+    :precondition: name_length is int, > 0
+    :postcondition: DnD character with attributes and name
+    :return: list
+    """
+
+    the_character = []
+
+    the_character.append(generate_name(name_length))
+
+    # create our attributes
+    str_list = ["Strength", roll_die(3, 6)]
+    dex_list = ["Dexterity", roll_die(3, 6)]
+    con_list = ["Constitution", roll_die(3, 6)]
+    int_list = ["Intelligence", roll_die(3, 6)]
+    wis_list = ["Wisdom", roll_die(3, 6)]
+    cha_list = ["Charisma", roll_die(3, 6)]
+
+    # add the attributes
+    the_character.append(str_list)
+    the_character.append(dex_list)
+    the_character.append(con_list)
+    the_character.append(int_list)
+    the_character.append(wis_list)
+    the_character.append(cha_list)
+
+    return the_character
+
+
+def print_character(character):
+    """
+    Output DnD character details.
+
+    :param character: list
+    :precondition: a character list generated using create_character()
+    :return: none
+    """
+
+    print("Behold, the mighty ")
+    print(character[0])
+
+
+def main():
+    print("###########EXECUTING###########")
+
+    my_list = []
+
+    # which_to_run = random.randint(1, 3)
+    # if which_to_run == 1:
+    #     print("Case 1")
+    #     my_list = choose_inventory(["rapier", "longsword", "sidesword", "dagger", "buckler", "baton", "cloak"], 8)
+    # elif which_to_run == 2:
+    #     print("Case 2")
+    #     my_list = choose_inventory(["rapier", "longsword", "sidesword", "dagger", "buckler", "baton", "cloak"], -1)
+    # elif which_to_run == 3:
+    #     print("Case 3")
+    # my_list = choose_inventory(["rapier", "longsword", "sidesword", "dagger", "buckler", "baton", "cloak"], 7)
+    # print(my_list)
+
+    print("*************NEXT******************")
+    # print(generate_name(5))
+
+    print("**************FINALLY***************")
+    char = create_character(8)
+    print_character(char)
+
+
+if __name__ == "__main__":
+    main()
