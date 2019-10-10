49a20941659f067db902b0d489b127cc76bec4fa
clintonbf <cfernandes24@my.bcit.ca>
2019-10-10 16:16:58 -0700

    Re-coded generate_name() unit tests

--- Lab05/test_generate_name.py
+++ Lab05/test_generate_name.py
@@ -4,12 +4,7 @@ from Lab05 import Lab_05
 
 class TestGenerate_name(TestCase):
 
-    def test_generate_name_returns_list(self):
-        test_list = Lab_05.generate_name(4)
-
-        self.assertIsInstance(test_list, list)
-
     def test_generate_name_correct_length(self):
         test_list = Lab_05.generate_name(4)
 
-        self.assertEqual(len(test_list), 4)
+        self.assertEqual(len(test_list), 8)
