457409cedb6eda3e1271e711b11ebbdf7a27b4fe
clintonbf <cfernandes24@my.bcit.ca>
2019-10-10 20:18:15 -0700

    code completion: test_print_character()

--- Lab05/test_print_character.py
+++ Lab05/test_print_character.py
@@ -1,5 +1,4 @@
 import io
-import sys
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -20,6 +19,28 @@ class TestPrint_character(TestCase):
                            "Intelligence: 4!\n"
                            "Wisdom: 5!\n"
                            "Charisma: 6!\n")
+        Lab_05.print_character(test_list)
+
+        self.assertEqual(mock_output.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_inventory_list(self, mock_output):
+        test_list = ["Gandalf", ["Strength", 1], ["Dexterity", 2], ["Constitution", 3], ["Intelligence", 4],
+                     ["Wisdom", 5], ["Charisma", 6], ["sidesword", "rapier", "dagger", "longsword",
+                                                      "cloak of 'what the hell is this?'"]]
+
+        expected_output = ("Gandalf\n"
+                           "Strength: 1!\n"
+                           "Dexterity: 2!\n"
+                           "Constitution: 3!\n"
+                           "Intelligence: 4!\n"
+                           "Wisdom: 5!\n"
+                           "Charisma: 6!\n"
+                           "sidesword\n"
+                           "rapier\n"
+                           "dagger\n"
+                           "longsword\n"
+                           "cloak of 'what the hell is this?'\n")
         Lab_05.print_character(test_list)
 
         self.assertEqual(mock_output.getvalue(), expected_output)
