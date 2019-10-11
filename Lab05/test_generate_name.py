6c82a234bcdeb8388216c4cc48da05f0ba0e8314
clintonbf <cfernandes24@my.bcit.ca>
2019-10-10 18:14:43 -0700

    Tested each character in the generated name

--- Lab05/test_generate_name.py
+++ Lab05/test_generate_name.py
@@ -5,6 +5,19 @@ from Lab05 import Lab_05
 class TestGenerate_name(TestCase):
 
     def test_generate_name_correct_length(self):
-        test_list = Lab_05.generate_name(4)
+        test_name = Lab_05.generate_name(4)
 
-        self.assertEqual(len(test_list), 8)
+        self.assertEqual(len(test_name), 8)
+
+    def test_evens_are_vowels(self):
+        test_name = Lab_05.generate_name(9)
+
+        for i in range(1, len(test_name), 2):
+            self.assertIn(test_name[i], ['a', 'e', 'i', 'o', 'u', 'y'])
+
+    def test_odds_are_consonants(self):
+        test_name = Lab_05.generate_name(9)
+
+        for i in range(0, len(test_name), 2):
+            self.assertIn(test_name[i].lower(), ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
+                                                 's', 't', 'v', 'w', 'x', 'y', 'z'])
