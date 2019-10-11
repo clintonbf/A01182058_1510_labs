32835cd0989cc36fd4d1a7e3d00a154aec95d5df
clintonbf <cfernandes24@my.bcit.ca>
2019-10-10 20:09:01 -0700

    Code creation: test_print_character() minor additions to print_character()

--- Lab05/test_print_character.py
+++ Lab05/test_print_character.py
@@ -1,6 +1,25 @@
+import io
+import sys
 from unittest import TestCase
+from unittest.mock import patch
+
+from Lab05 import Lab_05
 
 
 class TestPrint_character(TestCase):
-    def test_print_character(self):
-        self.fail()
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_character_attributes(self, mock_output):
+        test_list = ["Gandalf", ["Strength", 1], ["Dexterity", 2], ["Constitution", 3], ["Intelligence", 4],
+                     ["Wisdom", 5], ["Charisma", 6]]
+
+        expected_output = ("Gandalf\n"
+                           "Strength: 1!\n"
+                           "Dexterity: 2!\n"
+                           "Constitution: 3!\n"
+                           "Intelligence: 4!\n"
+                           "Wisdom: 5!\n"
+                           "Charisma: 6!\n")
+        Lab_05.print_character(test_list)
+
+        self.assertEqual(mock_output.getvalue(), expected_output)
