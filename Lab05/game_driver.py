bb5a4d8dbd69b85bdefba55ab3e24a699b90a9d0
clintonbf <cfernandes24@my.bcit.ca>
2019-10-10 17:15:02 -0700

    Finished code for game_driver() an corrected some code in Lab_05.py

--- Lab05/game_driver.py
+++ Lab05/game_driver.py
@@ -7,21 +7,29 @@ def main():
     syl_count = input("Forgot my glasses; can't read my attendance sheet..... how many syllables are in your name?")
     character = Lab_05.create_character(int(syl_count))
 
-    print("********** Welcome " + character[0] + "!**********")
+    print("********** Welcome " + character[0] + "! Announce yourself!**********")
 
-    eq_count = input("********** I have 4 items for you! How many you would like?********** ")
-    eq_count = int(eq_count) - 0  # enough of these f'ing string errors
-
-    items_list = ["rapier", "broadsword (which isn't what you think it is)", "vorpal sword (vorp!)",
-                  "Coding the gregarious's python"]
-
-    if eq_count != 0:
-        eq_list = Lab_05.choose_inventory(items_list, eq_count)
-
-    if len(eq_list) >= eq_count:
-        character.append(eq_list)
+    print("Behold! I am the mighty " + character[0] + " - scourge of a place with an equally incomprehensible name.\n")
+    print("My uproarious abilities:\n")
+    print('####################')
 
     Lab_05.print_character(character)
+
+    print('####################')
+
+    items_list = ["Rapier", "Broadsword", "Vorpal sword", "The constrictor", "A newt"]
+
+    eq_count = input("********** " + character[0] + ", I have " + str(
+        len(items_list)) + " items for you! But, they're secret. How many you would like?**********\n ")
+    eq_count = int(eq_count) - 0  # enough of these f'ing string errors
+
+    eq_list = []
+    if eq_count > len(items_list) or eq_count <= 0:
+        Lab_05.choose_inventory(items_list, eq_count)
+    elif eq_count > 0:
+        character.append(Lab_05.choose_inventory(items_list, eq_count))
+        print("********** You are a new person! Behold, world! I give you: **********\n ")
+        Lab_05.print_character(character)
 
 
 if __name__ == '__main__':
