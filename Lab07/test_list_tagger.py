c69e187775bc3ec4b4c8260725bad8205106474d
clintonbf <cfernandes24@my.bcit.ca>
2019-10-30 21:58:40 -0700

    More unit tests for list_tagger. For each DJE I've added - test for length - test on value for element 0

--- Lab07/test_list_tagger.py
+++ Lab07/test_list_tagger.py
@@ -19,7 +19,17 @@ class TestList_tagger(TestCase):
 
         self.assertEqual(len(lst), 2)
 
+    def test_list_tagger_1_item_list_supplied_has_element_0_eq_1(self):
+        lst = list_tagger(['a'])
+
+        self.assertEqual(lst[0], 1)
+
     def test_list_tagger_2_item_list_supplied(self):
         lst = list_tagger(['a', 'b'])
 
         self.assertEqual(len(lst), 3)
+
+    def test_list_tagger_2_item_list_supplied_has_element_0_eq_2(self):
+        lst = list_tagger(['a', 'b'])
+
+        self.assertEqual(lst[0], 2)
