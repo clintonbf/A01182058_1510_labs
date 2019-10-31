804488a144d48308586ecc3bc3554f48daa6798d
clintonbf <cfernandes24@my.bcit.ca>
2019-10-30 21:55:08 -0700

    Did I say that unit tests for list_tagger() was done? I meant that they're done now.

--- Lab07/test_list_tagger.py
+++ Lab07/test_list_tagger.py
@@ -5,18 +5,21 @@ from midterm import list_tagger
 
 class TestList_tagger(TestCase):
     def test_list_tagger_0_length_list_supplied(self):
-        lst = []
         lst = list_tagger([])
 
         self.assertEqual(len(lst), 1)
+
+    def test_list_tagger_0_length_list_supplied_has_0th_element_0(self):
+        lst = list_tagger([])
+
+        self.assertEqual(lst[0], 0)
+
+    def test_list_tagger_1_item_list_supplied(self):
+        lst = list_tagger(['a'])
+
+        self.assertEqual(len(lst), 2)
 
     def test_list_tagger_2_item_list_supplied(self):
         lst = list_tagger(['a', 'b'])
 
         self.assertEqual(len(lst), 3)
-
-    def test_list_tagger_0_length_list_supplied_has_0th_element_0(self):
-        lst = []
-        lst = list_tagger([])
-
-        self.assertEqual(lst[0], 0)
