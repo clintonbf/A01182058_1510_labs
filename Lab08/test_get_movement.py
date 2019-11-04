81237e824a3d8a2c7ccc743fc79eee7288b04211
clintonbf <cfernandes24@my.bcit.ca>
2019-11-04 14:10:32 -0800

    get_movement: removed shadow on dir

--- Lab08/test_get_movement.py
+++ Lab08/test_get_movement.py
@@ -7,6 +7,6 @@ from Lab08.maze import get_movement
 class TestGet_movement(TestCase):
     @patch('builtins.input', side_effect=['n'])
     def test_get_movement(self, mock_input):
-        dir = get_movement()
+        direction = get_movement()
 
-        self.assertEqual(dir, 'n')
+        self.assertEqual(direction, 'n')
