b79538167a45db1240ebd4a1e9ef74c9b7430a48
clintonbf <cfernandes24@my.bcit.ca>
2019-11-17 15:21:43 -0800

    refactored out the calorie output

--- Lab10/question_07.py
+++ Lab10/question_07.py
@@ -1,24 +1,42 @@
 # Global Constant
 _calories = {"lettuce": 5, "carrot": 52, "apple": 72, "bread": 66, "pasta": 221, "rice": 225, "milk": 122,
-             "cheese": 115,
-             "yogurt": 145, "beef": 240, "chicken": 140, "butter": 102}
+             "cheese": 115, "yogurt": 145, "beef": 240, "chicken": 140, "butter": 102}
 
-# Input loop
-new_item = input("Enter food item to add, or ’q’ to exit: ")
-while new_item != "q":
-    new_item_calories = int(input("Enter calories for " + new_item + ": "))
-    _calories[new_item] = new_item_calories
 
-    total_calories = 0
-    for item in _calories: total_calories = total_calories + _calories[item]
+def output_calorie_sum(lst: list):
+    """
+    Sum all the items in a list.
 
-    food_item_names = []
-    for item in _calories:
-        food_item_names.append(item)
+    :param lst: a list of integers
+    :precondition:  every item in the list is an int
+    :precondition: lst has > 0 items
+    :postcondition: sum of list items is calculated and output
 
-    avg_calories = total_calories / len(_calories)
+    >>> output_calorie_sum([1, 2])
+    Total calories: 3
+    """
+    print("Total calories:", sum(lst))
 
-    print("\nFood Items:", sorted(food_item_names))
-    print("Total Calories:", total_calories, "Average Calories: %0.1f\n" % avg_calories)
 
+def main():
+    # Input loop
     new_item = input("Enter food item to add, or ’q’ to exit: ")
+    while new_item != "q":
+        new_item_calories = int(input("Enter calories for " + new_item + ": "))
+        _calories[new_item] = new_item_calories
+
+        total_calories = 0
+        for item in _calories:  # Sum calories for all items
+            total_calories = total_calories + _calories[item]
+
+        food_item_names = []
+        for item in _calories:  # Create a list of just the item names (ie. the dictionary keys)
+            food_item_names.append(item)
+
+        avg_calories = total_calories / len(_calories)  # calculate average calories of the items
+
+        print("\nFood Items:", sorted(food_item_names))  # Output food names in alphabetical order
+        print("Total Calories:", total_calories,
+              "Average Calories: %0.1f\n" % avg_calories)  # print out total calories and average calories
+
+        new_item = input("Enter food item to add, or ’q’ to exit: ")
---
+++ Lab10/test_output_calorie_sum.py
@@ -0,0 +1,39 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch
+
+from Lab10.question_07 import output_calorie_sum
+
+
+class TestOutput_calorie_sum(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_output_calorie_sum_two_items_all_above_0_cal(self, mock_output):
+        expected_output = "Total calories: 3\n"
+
+        output_calorie_sum([1, 2])
+
+        self.assertEqual(mock_output.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_output_calorie_sum_1_item_0_cal(self, mock_output):
+        expected_output = "Total calories: 0\n"
+
+        output_calorie_sum([0])
+
+        self.assertEqual(mock_output.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_output_calorie_sum_1_item_gt_0_cal(self, mock_output):
+        expected_output = "Total calories: 5\n"
+
+        output_calorie_sum([5])
+
+        self.assertEqual(mock_output.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_output_calorie_sum_gt_1_item_with_1_item_of_0_cal(self, mock_output):
+        expected_output = "Total calories: 50\n"
+
+        output_calorie_sum([50, 0])
+
+        self.assertEqual(mock_output.getvalue(), expected_output)
